7ca8366141bdb6b93cb4ef0d3bd044fb7db8c828
Zach Selchau <zhselchau1998@yahoo.com>
2018-10-09 21:08:26 -0700

    Set up prog2_1.hpp

---
+++ prog2_1.hpp
@@ -0,0 +1,12 @@
+#ifndef __PROG2_1_HPP__
+#define
+
+    class Tokenizer{
+    
+        public:
+            Tokenizer();
+            ~Tokenizer();
+            void Tokenize(string s);
+            vector<string> GetTokens();
+    };
+#endif
