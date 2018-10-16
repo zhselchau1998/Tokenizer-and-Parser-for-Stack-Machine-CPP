7e8b3255fc95cb0ae017b2a0b5c48dbbd66f019a
Zach Selchau <zhselchau1998@yahoo.com>
2018-10-16 02:10:38 -0700

    Prog2_3.cpp finished. Tested and should work.

---
+++ prog2_3.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <exception>
+#include "prog2_3.hpp"
+
+using namespace std;
+
+Parser :: Parser(){
+
+}
+
+
+Parser :: ~Parser(){
+
+}
+
+
+bool Parser :: Parse(vector<string> inputs){
+    
+    string stest = "";
+    bool lengthTest=false;
+    bool testTwo = false;
+    int intTest = 0;
+
+    for (int i=0; i<inputs.size(); i++){
+
+        stest=inputs[i];
+        
+        //Vector Size Test
+        if(lengthTest)
+            return false;
+
+        //1 Token Long Test
+        if((stest.compare("pop")==0 || stest.compare("add")==0 || stest.compare("sub")==0 || stest.compare("mul")==0 || stest.compare("div")==0 || stest.compare("mod")==0 ||stest.compare("skip")==0) && i==0)
+            lengthTest=true;
+
+        //2 Token Long Test
+        if(stest.compare("push")==0 || stest.compare("save")==0 || stest.compare("get")==0)
+        if(i==0 && inputs.size()!=1)
+            testTwo=true;
+
+        //2 Token 2nd Test
+        if(testTwo && i==1 && stringstream(stest) >> intTest)
+            lengthTest=true;
+    }    
+    return lengthTest;
+}
+
+/*
+int main(){
+    
+    Parser *parser = new Parser();
+    vector<string> vect{"get", "12"};
+
+    if(parser->Parse(vect))
+        cout << "Success!\n";
+    else
+        cout << "Fail.\n";
+        
+}*/
+
+
+
